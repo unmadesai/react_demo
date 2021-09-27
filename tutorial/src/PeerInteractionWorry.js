a135e234cc20843b9bf8392be2dd4d09072c0643
unmadesai <unma96@gmail.com>
2021-09-27 02:55:35 -0700

    modifications for missing data

--- tutorial/src/PeerInteractionWorry.js
+++ tutorial/src/PeerInteractionWorry.js
@@ -9,17 +9,24 @@ class PeerInteractionWorry extends React.Component {
     
       series: [{
         name: 'Morning',
+        type: 'bar',
         data: [0,4,5,61,0,0,65,62,0,58,66,0,0,50]
       }, {
         name: 'Afternoon',
-        data: [6,6,7,0,50,68,66,63,65,67,50,0,50,0]
+        type: 'bar',
+        data: [6,6,7,0,0,68,66,63,65,67,50,0,50,0]
       }, {
         name: 'Evening',
-        data: [6,5,35,0,50,66,70,0,0,66,50,50,49,65]
-      }],
+        type: 'bar',
+        data: [6,5,35,0,0,66,70,0,0,66,50,50,49,65]
+      }, {
+        type: 'scatter',
+        name: 'No data',
+      data: [,,,,10,,,,,,,,,]
+    }],
       options: {
         chart: {
-          type: 'bar',
+          //type: 'bar',
           zoom: {
             enabled: false
           },
@@ -44,8 +51,8 @@ class PeerInteractionWorry extends React.Component {
         },
         stroke: {
           show: true,
-          width: 2,
-          colors: ['transparent']
+          width: 1,
+          colors: ['#fff']
         },
         xaxis: {
           categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 
@@ -55,8 +62,21 @@ class PeerInteractionWorry extends React.Component {
           max: 100
         },
         colors: ['#B192B5', '#89608E', '#68496E'],
+        markers: {
+          size: [0, 0, 0, 8],
+          strokeWidth: 0,
+          hover: {
+            sizeOffset: 0
+          }
+        },
         fill: {
-          opacity: 1
+          type: ['solid', 'solid', 'solid', 'image'],
+          opacity: 1,
+          image: {
+              src: ['images/worry_certainty/nodata.png'],
+              width: 10,
+              height: 10
+          },
         },
       },
     };
@@ -66,7 +86,7 @@ class PeerInteractionWorry extends React.Component {
 render() {
     return (
         <div id="chart">
-            <Chart options={this.state.options} series={this.state.series} type="bar" height={250} style={{marginLeft: "3em", marginRight: "0.3em" }}/>
+            <Chart options={this.state.options} series={this.state.series} type="bar" height={300} style={{marginLeft: "3em", marginRight: "0.3em" }}/>
         </div>
     );
   }
